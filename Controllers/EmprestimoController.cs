2d626eef9d8617a72b935bf92e880e5d90bf1f10
Willim-DEV <95634114+Willim-DEV@users.noreply.github.com>
2021-12-29 15:20:13 -0300

    restante das atualizações

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -16,13 +16,16 @@ namespace Biblioteca.Controllers
             EmprestimoService emprestimoService = new EmprestimoService();
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+
+            cadModel.Livros = livroService.ListarDisponiveis();
             return View(cadModel);
         }
 
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario))
+            {
             EmprestimoService emprestimoService = new EmprestimoService();
             
             if(viewModel.Emprestimo.Id == 0)
@@ -35,11 +38,26 @@ namespace Biblioteca.Controllers
             }
             return RedirectToAction("Listagem");
         }
+        else
+        {
+                ViewData["mensagem"] = "Por favor, Preencha todos os campos";
+
+                LivroService livroService = new LivroService();
+                EmprestimoService emprestimoService = new EmprestimoService();
+
+                CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+
+                cadModel.Livros = livroService.ListarDisponiveis();
+
+                return View(cadModel);
+        }
+    }
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
             Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
+
             if(!string.IsNullOrEmpty(filtro))
             {
                 objFiltro = new FiltrosEmprestimos();
@@ -58,7 +76,8 @@ namespace Biblioteca.Controllers
             Emprestimo e = em.ObterPorId(id);
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            cadModel.Livros = livroService.ListarDisponiveis();
+            cadModel.Livros.Add(livroService.ObterPorId(e.LivroId));
             cadModel.Emprestimo = e;
             
             return View(cadModel);
